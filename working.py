14cf346bde6bc33b1bbb535fc0efb1011b40f927
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-01-15 19:34:15 +0500

    final1

--- working.py
+++ working.py
@@ -12,20 +12,20 @@ def video_scrapper(url):
     options.add_argument("--no-sandbox")
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
-    driver.get(url)
+    x = driver.get(url)
     
     
     username = driver.find_element_by_id('user_login')
-    username.send_keys('george_gina4med')
+    x = username.send_keys('george_gina4med')
     password = driver.find_element_by_id('user_password')
-    password.send_keys('Akoznaeh#88')
+    x = password.send_keys('Akoznaeh#88')
     form = driver.find_element_by_id('new_user')
-    form.submit()
+    x = form.submit()
 
     res = driver.execute_script("return document.documentElement.outerHTML")
     soup = BeautifulSoup(res, 'lxml')    
 
-    driver.quit()
+    x = driver.quit()
     html_str = str(soup)
     start = end = 0
     start = html_str.find("//<![CDATA[")+43
