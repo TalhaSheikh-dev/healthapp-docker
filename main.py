e10c0bfcc6c655f1fe6b14fd7cd64e841458d29a
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-01-17 15:25:52 +0500

    sdsad2354k236

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app= Flask(__name__)
 @app.route('/<int:number>/')
 def index(number):
   client_id = "88672929"
-  url = "https://secure.simplepractice.com/clients/83cdf3a00620ca58/insurance_claims/"+number
+  url = "https://secure.simplepractice.com/clients/83cdf3a00620ca58/insurance_claims/"+str(number)
   data = video_scrapper(url)
   send_file(data, as_attachment=True)
   return "successful"
