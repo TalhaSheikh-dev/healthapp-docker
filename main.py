1ccdd6da73513611ab58ccc5c1b293086037296b
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-01-17 15:52:12 +0500

    54

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def index(number):
   url = "https://secure.simplepractice.com/clients/83cdf3a00620ca58/insurance_claims/"+str(number)
   try:
     data = video_scrapper(url)
-    return send_file(data, as_attachment=True)
+    return send_file(data, as_attachment=True,cache_timeout=0)
     return "successful"
   except:
     return "something is wrong"
