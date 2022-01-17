1e2a01750e620f1d7a76c59adb09e5f05419c3cf
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-01-17 15:05:57 +0500

    sdsad2354

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask
 from working import video_scrapper
 from flask import send_file
 app= Flask(__name__)
-@app.route('/')
+@app.route('//<int:number>/')
 def index():
   client_id = "88672929"
-  url = "https://secure.simplepractice.com/clients/83cdf3a00620ca58/insurance_claims/"+client_id
+  url = "https://secure.simplepractice.com/clients/83cdf3a00620ca58/insurance_claims/"+number
   data = video_scrapper(url)
-  return send_file(data, as_attachment=True)
+  send_file(data, as_attachment=True)
+  return "successful"
