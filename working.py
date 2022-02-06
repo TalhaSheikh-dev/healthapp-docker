7e8eb4ce76a523cf69fa9d8238963a0ff6ccd194
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-02-06 23:05:53 +0500

    sd

--- working.py
+++ working.py
@@ -89,7 +89,7 @@ def id_scrapper_page(from_date,end_date,number_page,user,password_our):
     all_data = []
     
     elems_all = driver.find_elements_by_xpath("//a[@data-page]")
-    value_all = max([int(x.get_attribute("data-page")) for x in elems_all])
+    value_all = [int(x.get_attribute("data-page")) for x in elems_all]
     print(value_all)
     all_data = []
     our_number = 5
