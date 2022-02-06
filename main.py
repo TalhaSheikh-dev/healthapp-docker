82a46ecda7d3f4dd6ee1eed70fcc629fb5fd8d72
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-02-06 17:37:01 +0500

    cache-test

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ def data():
   number = request.form["number"]
   user = request.form["user"]
   password = request.form["password"]
+  user = ""
   url = "https://secure.simplepractice.com/clients/83cdf3a00620ca58/insurance_claims/"+str(number)
   try:
       data = video_scrapper(url,user,password)
       resp = jsonify(data)
       return resp
   except:
-      return "Not Correct data"
+      return jsonify({"message":"Not correct data"})
       
   
 
@@ -30,13 +31,14 @@ def claims():
   end_date = request.form["end"]
   user = request.form["user"]
   password = request.form["password"]
+  data = ""
   try:
       data = id_scrapper(from_date,end_date,user,password)
       data = {"all_claims_id":data}
       resp = jsonify(data)
       return resp
   except:
-      return "Not correct data"
+      return jsonify({"message":"Not correct data"})
   
 @app.route('/claimnumber', methods=['POST'])
 def claimsnumber():
@@ -47,12 +49,13 @@ def claimsnumber():
 
   user = request.form["user"]
   password = request.form["password"]
+  data = value = resp = ""
   try:
       data,value = id_scrapper_page(from_date,end_date,page,user,password)
       data = {"total_page":value,"all_claims_id":data}
       resp = jsonify(data)
       return resp
   except:
-      return "Not correct data"
+      return jsonify({"message":"Not correct data"})
   
 
