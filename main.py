bdcac02d090dc69ba3d3e546659542833f7718e7
TalhaSheikh-dev <talhasheikh9292@gmail.com>
2022-09-02 11:19:29 +0500

    make it better

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ def data():
   password = request.form["password"]
   url = "https://secure.simplepractice.com/clients/"+str(first_number)+"/insurance_claims/"+str(second_number)
   #return jsonify(video_scrapper(url,user,password))
-  try:
-      return jsonify(video_scrapper(url,user,password))
+  #try:
+  return jsonify(video_scrapper(url,user,password))
       
-  except:
-      return jsonify({"message":"Not correct data"})
+  #except:
+  #    return jsonify({"message":"Not correct data"})
       
       
       
