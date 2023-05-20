520010668450f23db33c602dd327e9337bc68fd1
Bill Khan <collabmvp@gmail.com>
2023-05-21 01:31:59 +0500

    new endpoint

--- working.py
+++ working.py
@@ -182,6 +182,7 @@ def get_all_client(user,password_our):
     while True:
         response = driver.get(url+str(i)+url_2)
         json_data = driver.find_element_by_tag_name("body").text
+        print(json_data)
         json_data = json.loads(json_data)["data"]
         if len(json_data) == 0:
             break
