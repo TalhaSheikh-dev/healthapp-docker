7dc950d16063f9e5713ded0f2e626823c158cb8c
TalhaSheikh-dev <talhakhawar9292@gmail.com>
2024-07-31 21:52:47 +0500

    slvddv changes

--- working.py
+++ working.py
@@ -226,9 +226,8 @@ def unbilled_create(from_date,end_date,user,password_our):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.add_argument("window-size=1400,900")
-    driver = webdriver.Chrome(options=options)
-    # options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
     driver.get("https://secure.simplepractice.com/users/sign_in")
     
   
