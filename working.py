5805ad61e4330ecd29f16c52fa651343a463d990
TalhaSheikh-dev <talhakhawar9292@gmail.com>
2024-12-24 16:09:05 +0500

    changes

--- working.py
+++ working.py
@@ -275,6 +275,7 @@ def unbilled_create(from_date,end_date,user,password_our):
     
     payload = json.dumps({"appointmentIds":all_ids,"submitClaims":False,"updateAllBillingZipCodes":False})    
     r = requests.post("https://secure.simplepractice.com/frontend/insured-clients/batch-create",data=payload,headers=header)
+    print(r.status_code)
     if r.status_code == 201:
         return "successful"
     else:
